d24733398ce6dac2915195bd75bec7ac21ca48c8
El Hadj Mbengue Arroyo <109061270+arroyostack@users.noreply.github.com>
2024-01-30 11:28:34 +0100

    Update ReactCodeEditorTemplate.ts

--- src/html-boilerplate/ReactCodeEditorTemplate.ts
+++ src/html-boilerplate/ReactCodeEditorTemplate.ts
@@ -3,22 +3,11 @@ export const reactCodeTemplate = `
     import React, { useEffect, useState } from 'react';
     import ReactDOM from 'react-dom';
 
-    const URL = 'https://jsonplaceholder.typicode.com/todos/1';
-
     export const App = () => {
-
-        const [count, setCount] = useState(0);
-
-        useEffect( () => {
-            console.log("Hello World!.")
-        }, [] );
         
         return (
           <div>
-            <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
-              Click me
-            </button>
+            <h2>Hi Mars!</h2>
           </div>
         );
     };
